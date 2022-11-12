167914c814e6e05d789f4d486d9be3bb845efffa
hoscho <jgeist@gmx.net>
2022-11-12 11:15:48 +0100

    add simple JFrame to Application by start Application over interface main method

--- src/main/java/de/jgsoftwares/guiserverpanel/GUIServerPanel.java
+++ src/main/java/de/jgsoftwares/guiserverpanel/GUIServerPanel.java
@@ -2,8 +2,26 @@ package de.jgsoftwares.guiserverpanel;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.swing.*;
+
+import javax.swing.JFrame;
+import java.awt.*;
+
 @SpringBootApplication
 public class GUIServerPanel implements i_GuiServerPanel
 {
 
+
+
+    public static void showFrame(JFrame frame)
+    {
+            frame = new JFrame();
+            frame.setTitle("GUI Server Panel");
+            frame.setSize(640,400);
+            frame.setVisible(true);
+    }
+
+
+    public GUIServerPanel() {
+    }
 }
--- src/main/java/de/jgsoftwares/guiserverpanel/i_GuiServerPanel.java
+++ src/main/java/de/jgsoftwares/guiserverpanel/i_GuiServerPanel.java
@@ -1,13 +1,33 @@
+
 package de.jgsoftwares.guiserverpanel;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import javax.swing.*;
+import javax.swing.JFrame;
+
 public interface i_GuiServerPanel
 {
+
     public static void main(String[] args)
     {
+
+        try
+        {
+            JFrame frame = null;
+            GUIServerPanel.showFrame(frame);
+        } catch(Exception e)
+        {
+            System.out.print("Fehler" + e);
+        }
+
         SpringApplication.run(GUIServerPanel.class, args);
+
+
     }
+
+
+
 }
