c693ed7914629307baf86ffd1b7dfb5ffbf22c7c
hoscho <jgeist@gmx.net>
2023-01-23 15:26:20 +0100

    .

--- src/main/java/jrdesktop/server/Config.java
+++ src/main/java/jrdesktop/server/Config.java
@@ -5,7 +5,7 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.util.Properties;
 
-import jrdesktop.main;
+//import jrdesktop.main;
 import jrdesktop.utilities.InetAdrUtility;
 
 /**
@@ -20,36 +20,11 @@ public class Config {
     public static boolean default_address = false;
     
     public static void loadConfiguration() {
-        if (new File(main.SERVER_CONFIG_FILE).canRead())
-            try {
-                Properties properties = new Properties();            
-                properties.load(new FileInputStream(main.SERVER_CONFIG_FILE));
-                
-                server_address = properties.get("server-address").toString(); 
-                server_port = Integer.valueOf(properties.get("server-port").toString());                                          
-                
-                default_address = Boolean.valueOf(properties.getProperty("default-address"));
-            }
-            catch (Exception e) {
-                e.getStackTrace();
-            }
-       else
-            storeConfiguration();  
+   
     }
     
     public static void storeConfiguration () {
-        try {
-            new File(main.SERVER_CONFIG_FILE).createNewFile();        
-            Properties properties = new Properties();
-            properties.put("server-address", server_address);
-            properties.put("server-port", String.valueOf(server_port));        
-            properties.put("default-address", String.valueOf(default_address));
-        
-            properties.store(new FileOutputStream(main.SERVER_CONFIG_FILE), 
-                "jrdesktop server configuration file"); 
-        } catch (Exception e) {
-            e.getStackTrace();
-        }            
+      
     }    
     
     public static void SetConfiguration(int Port) { 
