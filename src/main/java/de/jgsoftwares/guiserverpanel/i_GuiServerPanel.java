6b113f9f4cc40f81bdff89f886fe8a2001a2224a
hoscho <jgeist@gmx.net>
2023-01-28 14:01:44 +0100

    .

--- src/main/java/de/jgsoftwares/guiserverpanel/i_GuiServerPanel.java
+++ src/main/java/de/jgsoftwares/guiserverpanel/i_GuiServerPanel.java
@@ -4,16 +4,11 @@ package de.jgsoftwares.guiserverpanel;
 public interface i_GuiServerPanel
 {
 
-
-
     public static void main(String[] args)
     {
 
         GUIServerPanel.showFrame();
-
        // SpringApplication.run(GUIServerPanel.class, args);
-
-
     }
 
 
