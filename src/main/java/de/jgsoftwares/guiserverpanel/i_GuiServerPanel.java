0417049297d77d3f50c630e91fb5eb13d5da43d3
hoscho <jgeist@gmx.net>
2024-08-04 14:57:10 +0200

    edit Guiserverpanel setting for openjdk11
    start main method over interface

--- src/main/java/de/jgsoftwares/guiserverpanel/i_GuiServerPanel.java
+++ src/main/java/de/jgsoftwares/guiserverpanel/i_GuiServerPanel.java
@@ -4,13 +4,13 @@ package de.jgsoftwares.guiserverpanel;
 public interface i_GuiServerPanel
 {
 
-    public static void main(String[] args)
+  
+public static void main(String[] args)
     {
 
         GUIServerPanel.showFrame();
        // SpringApplication.run(GUIServerPanel.class, args);
     }
-
 
  
 
