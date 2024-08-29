865e446cc125e58e8da517cf7af0a58753e06dc2
hoscho <jgeist@gmx.net>
2024-08-29 10:32:49 +0200

    edit openwrt form and buttons

--- src/main/java/de/jgsoftwares/guiserverpanel/frames/OpenWrt.java
+++ src/main/java/de/jgsoftwares/guiserverpanel/frames/OpenWrt.java
@@ -51,7 +51,7 @@ public class OpenWrt extends javax.swing.JPanel {
         jTextArea1.setRows(5);
         jScrollPane1.setViewportView(jTextArea1);
 
-        jButton1.setText("openwrt dmz");
+        jButton1.setText("openwrt dmz - cloudflare");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton1ActionPerformed(evt);
@@ -62,7 +62,7 @@ public class OpenWrt extends javax.swing.JPanel {
         jTextArea2.setRows(5);
         jScrollPane2.setViewportView(jTextArea2);
 
-        jButton5.setText("openwrt open dn");
+        jButton5.setText("openwrt open dns");
         jButton5.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton5ActionPerformed(evt);
